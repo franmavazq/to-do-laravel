e5f763d3f9e3f8a8d6011d3f5b7ddfbba6369184
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2019-08-06 08:33:38 -0400

    first change complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    To-do
+                    To-do Francisco M.
                 </div>
 
             </div>
